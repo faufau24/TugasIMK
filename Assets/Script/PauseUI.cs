77e304227b6ccd1af430970e63bf21540b1e5b86
FauFau <faufau241@gmail.com>
2022-04-12 22:51:08 +0700

    tugas 3

---
+++ Assets/Script/PauseUI.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PauseUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Controls _controls;
+    public GameObject pauseMenu; 
+    public bool isPaused;
+
+	private void Awake()
+    {
+        _controls = new Controls();
+    }
+    public void ExitGame(){
+        Application.Quit();
+    }
+
+    private void OnEnable()
+    {
+        _controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _controls.Disable();
+    }    
+    public void ResumeGame()
+    {
+		Cursor.lockState = CursorLockMode.Locked;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    
+    public void PauseGame()
+    {
+		Cursor.lockState = CursorLockMode.None;
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }    
+
+    public void BackMenu(string menuScene)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(menuScene);
+    }
+
+	public void Update(){
+		if (_controls.Player.Pause.IsPressed()){
+            if (isPaused){
+                ResumeGame();
+            } else {
+                PauseGame();
+            }
+        }
+	}
+}
---
+++ Assets/Script/UI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    private Controls _controls;
+    public void ExitGame(){
+        Application.Quit();
+    }
+
+    public void ChangeScene(string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+
+	private void Awake()
+    {
+        _controls = new Controls();
+    }
+
+    private void OnEnable()
+    {
+        _controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _controls.Disable();
+    }
+}
